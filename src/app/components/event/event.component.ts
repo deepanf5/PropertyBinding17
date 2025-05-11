da2d1d4da5b6d491cb84c1ea50eb329a40dd819d
deepan <deepanappu1999@gmail.com>
2025-05-11 14:02:11 +0530

    adding event binding

---
+++ src/app/components/event/event.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event',
+  standalone: true,
+  imports: [],
+  templateUrl: './event.component.html',
+  styleUrl: './event.component.css'
+})
+export class EventComponent {
+
+  text:string = ''
+
+  Onclick() {
+    this.text = "btton clicked"
+  }
+
+}
