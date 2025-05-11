43c08854274a0fc91a3c4f826447bbb5cb23d19f
deepan <deepanappu1999@gmail.com>
2025-05-11 11:52:03 +0530

    adding interpolation comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'propertyBinding17';
+  title:string = 'propertyBinding17';
+  imageUlr:string = "https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=1200"
+  imageAlt:string = "Graphic Image";
+  imageTitle:string = "Graphic Image" 
 }
