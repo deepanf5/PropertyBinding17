6516d3361992c051ed79f5034c47c71d0f6cf685
deepan <deepanappu1999@gmail.com>
2025-05-11 14:05:42 +0530

    importing all the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PropertyBindingComponent } from './components/property-binding/property-binding.component';
+import { InterpolationComponent } from './components/interpolation/interpolation.component';
+import { TwoWayComponent } from './components/two-way/two-way.component';
+import { AttributeComponent } from './components/attribute/attribute.component';
+import { EventComponent } from './components/event/event.component';
+import { StyleClassComponent } from './components/style-class/style-class.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    InterpolationComponent,
+    PropertyBindingComponent,
+    TwoWayComponent,
+    AttributeComponent,
+    EventComponent,
+    StyleClassComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title:string = 'propertyBinding17';
-  imageUlr:string = "https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=1200"
-  imageAlt:string = "Graphic Image";
-  imageTitle:string = "Graphic Image" 
+
 }
