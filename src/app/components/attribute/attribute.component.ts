30b87990321755105257ea603462c28522a16139
deepan <deepanappu1999@gmail.com>
2025-05-11 14:01:43 +0530

    adding attribute bindng

---
+++ src/app/components/attribute/attribute.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attribute',
+  standalone: true,
+  imports: [],
+  templateUrl: './attribute.component.html',
+  styleUrl: './attribute.component.css'
+})
+export class AttributeComponent {
+  btnValue:boolean = true;
+
+}
